691ef0d204aa24d47e32c5a38924acfced698e3a
PengAn Zhou <pengan1987@msn.com>
2013-08-23 14:50:53 -0600

    modify SNMP demo to working for TSL2561 sensor

--- snmp_example/TSL2561_SNMP.ino
+++ snmp_example/TSL2561_SNMP.ino
@@ -11,14 +11,9 @@
 #include <MemoryFree.h>
 #include <Agentuino.h>
 #include <Flash.h>
-#include <Servo.h>
+#include <TSL2561.h>
 //
-//Pin defnition
-int led3 = 3;
-int led5 = 5;
-int led6 = 6;
-int sysled = 9;
-Servo myservo;
+
 //network properties
 static byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 static byte ip[] = { 192, 168, 0, 64 };
@@ -36,22 +31,21 @@ static byte subnet[] = { 255, 255, 255, 0 };
 
 //definite a OID for our controler item
 static char sysLight[] PROGMEM = "1.3.6.1.4.1.36582.1.0";
-static char ledLight3[] PROGMEM = "1.3.6.1.4.1.36582.1.3";
-static char ledLight5[] PROGMEM = "1.3.6.1.4.1.36582.1.5";
-static char ledLight6[] PROGMEM = "1.3.6.1.4.1.36582.1.6";
+static char sysLux2561[] PROGMEM = "1.3.6.1.4.1.36582.2.0";
 static char sysIpAddr[] PROGMEM = "1.3.6.1.2.1.4.20.1.1";
 //
 // RFC1213 local values
 
-static int16_t sysLightOn = 2;
-static int16_t ledLightOn3 = 2;
-static int16_t ledLightOn5 = 2;
-static int16_t ledLightOn6 = 2;
+static int32_t locLight = 2;
 
 //SNMP resvered variables
 char oid[SNMP_MAX_OID_LEN];
 SNMP_API_STAT_CODES api_status;
 SNMP_ERR_CODES status;
+
+//Hardware Variables
+TSL2561 tsl(TSL2561_ADDR_FLOAT);
+int getLux();
 
 void pduReceived() {
 	SNMP_PDU pdu;
@@ -71,81 +65,47 @@ void pduReceived() {
 			// handle sysLight (set/get) requests
 			if (pdu.type == SNMP_PDU_SET) {
 				//make a temp pointer to parse pdu value
-				int16_t* tempvalue = &sysLightOn;
+				int32_t* tempvalue = &locLight;
 				status = pdu.VALUE.decode(tempvalue);
 
 				pdu.type = SNMP_PDU_RESPONSE;
 				pdu.error = status;
 			} else {
-				// response packet from get-request - locDescr
-				status = pdu.VALUE.encode(SNMP_SYNTAX_INT, sysLightOn);
+
+				status = pdu.VALUE.encode(SNMP_SYNTAX_INT, locLight);
 				pdu.type = SNMP_PDU_RESPONSE;
 				pdu.error = status;
 			}
 			//
 		} else if (strcmp_P(oid, sysIpAddr) == 0) {
 
-			// handle sysDescr (set/get) requests
 			if (pdu.type == SNMP_PDU_SET) {
-				// response packet from set-request - object is read-only
+
 				pdu.type = SNMP_PDU_RESPONSE;
 				pdu.error = SNMP_ERR_READ_ONLY;
 			} else {
-				// response packet from get-request - locDescr
+
 				status = pdu.VALUE.encode(SNMP_SYNTAX_IP_ADDRESS, ip);
 				pdu.type = SNMP_PDU_RESPONSE;
 				pdu.error = status;
 			}
-		} else if (strcmp_P(oid, ledLight3) == 0) {
-			// handle sysLight (set/get) requests
+		} else if (strcmp_P(oid, sysLux2561) == 0) {
+
 			if (pdu.type == SNMP_PDU_SET) {
-				//make a temp pointer to parse pdu value
-				int16_t* tempvalue = &ledLightOn3;
-				status = pdu.VALUE.decode(tempvalue);
 
 				pdu.type = SNMP_PDU_RESPONSE;
-				pdu.error = status;
+				pdu.error = SNMP_ERR_READ_ONLY;
 			} else {
-				// response packet from get-request - locDescr
-				status = pdu.VALUE.encode(SNMP_SYNTAX_INT, ledLightOn3);
+				//get the lux value from sensor
+				int lux2561;
+				lux2561 = getLux();
+				status = pdu.VALUE.encode(SNMP_SYNTAX_INT, lux2561);
 				pdu.type = SNMP_PDU_RESPONSE;
 				pdu.error = status;
 			}
 		}
 
-		else if (strcmp_P(oid, ledLight5) == 0) {
-			// handle sysLight (set/get) requests
-			if (pdu.type == SNMP_PDU_SET) {
-				//make a temp pointer to parse pdu value
-				int16_t* tempvalue = &ledLightOn5;
-				status = pdu.VALUE.decode(tempvalue);
-
-				pdu.type = SNMP_PDU_RESPONSE;
-				pdu.error = status;
-			} else {
-				// response packet from get-request - locDescr
-				status = pdu.VALUE.encode(SNMP_SYNTAX_INT, ledLightOn5);
-				pdu.type = SNMP_PDU_RESPONSE;
-				pdu.error = status;
-			}
-		}
-
-		else if (strcmp_P(oid, ledLight6) == 0) {
-			// handle sysLight (set/get) requests
-			if (pdu.type == SNMP_PDU_SET) {
-				//make a temp pointer to parse pdu value
-				int16_t* tempvalue = &ledLightOn6;
-				status = pdu.VALUE.decode(tempvalue);
-
-				pdu.type = SNMP_PDU_RESPONSE;
-				pdu.error = status;
-			} else {
-				// response packet from get-request - locDescr
-				status = pdu.VALUE.encode(SNMP_SYNTAX_INT, ledLightOn6);
-				pdu.type = SNMP_PDU_RESPONSE;
-				pdu.error = status;
-			}
-		} else {
+		else {
 			// oid does not exist
 			//
 			// response packet - object not found
@@ -162,12 +122,22 @@ void pduReceived() {
 }
 
 void setup() {
-	pinMode(sysled, OUTPUT);
-	myservo.attach(led3);
-	pinMode(led5, OUTPUT);
-	pinMode(led6, OUTPUT);
-
+	pinMode(9, OUTPUT);
 	Serial.begin(9600);
+
+	//hardware initialize
+
+	if (tsl.begin()) {
+		Serial.println("Found sensor");
+	} else {
+		Serial.println("No sensor?");
+		while (1)
+			;
+	}
+	tsl.setGain(TSL2561_GAIN_16X);
+	tsl.setTiming(TSL2561_INTEGRATIONTIME_13MS);
+
+	//Start Ethernet
 	Ethernet.begin(mac);
 	//print local ip address on console
 	Serial.print("My IP address: ");
@@ -205,36 +175,42 @@ void loop() {
 	//
 	//
 
-	if (sysLightOn == 1) {
-		digitalWrite(sysled, HIGH);
-	} else if (sysLightOn == 2) {
-		digitalWrite(sysled, LOW);
-
-	}
-
-	/*if (ledLightOn3 == 1) {
-	 digitalWrite(led3, HIGH);
-	 } else if (ledLightOn3 == 2) {
-	 digitalWrite(led3, LOW);
-
-	 }*/
-	if (ledLightOn3 >= 0 && ledLightOn3 < 180) {
-		myservo.write(ledLightOn3);
-	}
-
-	if (ledLightOn5 == 1) {
-		digitalWrite(led5, HIGH);
-	} else if (ledLightOn5 == 2) {
-		digitalWrite(led5, LOW);
-
-	}
-
-	if (ledLightOn6 == 1) {
-		digitalWrite(led6, HIGH);
-	} else if (ledLightOn6 == 2) {
-		digitalWrite(led6, LOW);
+	if (locLight == 1) {
+		digitalWrite(9, HIGH);
+	} else if (locLight == 2) {
+		digitalWrite(9, LOW);
 
 	}
 
 }
 
+int getLux() {
+	uint16_t x = tsl.getLuminosity(TSL2561_VISIBLE);
+	//uint16_t x = tsl.getLuminosity(TSL2561_FULLSPECTRUM);
+	//uint16_t x = tsl.getLuminosity(TSL2561_INFRARED);
+
+	Serial.println(x, DEC);
+
+	// More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
+	// That way you can do whatever math and comparisons you want!
+	uint32_t lum;
+	uint16_t ir, full;
+	lum = tsl.getFullLuminosity();
+
+	ir = lum >> 16;
+	full = lum & 0xFFFF;
+	Serial.print("IR: ");
+	Serial.print(ir);
+	Serial.print("\t\t");
+	Serial.print("Full: ");
+	Serial.print(full);
+	Serial.print("\t");
+	Serial.print("Visible: ");
+	Serial.print(full - ir);
+	Serial.print("\t");
+
+	Serial.print("Lux: ");
+	int lux = tsl.calculateLux(full, ir);
+	Serial.println(lux);
+	return lux;
+}
